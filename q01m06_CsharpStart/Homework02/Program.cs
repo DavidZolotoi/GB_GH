79c52a2c906ab6b8acff3b8f3aba11af9a7a1977
David <zolotoi77@inbox.ru>
2022-12-02 17:31:02 +0300

    Д/З по знакомству с языками. Урок 2 - Базовые алгоритмы. Сделал задачу №7.

--- q01m06_CsharpStart/Homework02/Program.cs
+++ q01m06_CsharpStart/Homework02/Program.cs
@@ -161,23 +161,10 @@ void SolutionTask7()
 {
     System.Console.WriteLine($"Выбрана задача № 7.");
     int number = InputInteger("Для решения задачи необходимо ввести число.\nВведите число: ");
-    int count = 0;
-    while(true)
-    {
-        number /= 10;
-        count++;
-        if (count<2 && number==0)
-        {
-            System.Console.WriteLine($"У числа {number} нет третьей цифры.");
-            break;
-        }
-        if (count==2 && number!=0)
-        {
-            System.Console.WriteLine($"{}");
-            break;
-        }
-    }
-    
+    if (number / 100 == 0)
+        System.Console.WriteLine($"В числе {number} нет третьей цифры.");
+    else
+        System.Console.WriteLine($"В числе {number}, третья цифра: {number % 10}.");
 };
 // Метод решения задачи № 8
 void SolutionTask8()
