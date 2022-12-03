0781fce6963ddac2c49d28a1a8a6b20dee1498b5
David <zolotoi77@inbox.ru>
2022-12-03 12:16:05 +0300

    K/P по знакомству с языками. Урок 4 - Функции. Генерация и вывод массива, поиск max1 и max2 в массиве.

---
+++ q01m06_CsharpStart/Classwork04_01/Program.cs
@@ -0,0 +1,35 @@
+﻿// Решение в группах задач:
+// Задача 3: Напишите программу, которая выводит 
+// массив из 8 элементов, заполненный нулями и единицами в случайном порядке.
+// [1,0,1,1,0,1,0,0]
+
+int range = Promt("Введите длину массива ");
+int minValue = Promt("Введите минимальное значение ");
+int maxValue = Promt("Введите максимальное значение ");
+int [] arr = GenerateArray(range, minValue, maxValue);
+PrintArray(arr);
+
+int[] GenerateArray(int Length, int minRange, int maxRange)
+{
+
+    int[] array = new int[Length];
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(minRange, maxRange+1);
+    }
+    return array;
+}
+
+void PrintArray(int[] array)
+{
+    for (int j = 0; j < array.Length; j++)
+    {
+        Console.Write(array[j] + " ");
+    }
+}
+
+int Promt(String message)
+{
+    System.Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
