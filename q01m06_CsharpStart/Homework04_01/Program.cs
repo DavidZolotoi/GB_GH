03918eea421a581c357119206fa9a11315591ea8
David <zolotoi77@inbox.ru>
2022-12-04 21:18:05 +0300

    Д/З по знакомству с языками. Урок 4 - Функции. Сделал задачу №1.

---
+++ q01m06_CsharpStart/Homework04_01/Program.cs
@@ -0,0 +1,31 @@
+﻿Console.WriteLine("Задача 1: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B. Реализовать через функции.");
+
+int A = InputInteger("Введите число A");
+int B = InputInteger("Введите число B");
+long result = PowerAtoB(A, B);
+string resultMess = (result == -1) ?
+        $"Вы ввели не натуральную степень: {B}"
+        :
+        $"{A} в степени {B} равно {result}";
+System.Console.WriteLine(resultMess);
+
+// Метод возведения числа A в число B
+long PowerAtoB(int A, int B)
+{
+    if (B <= 0) return -1;
+    long result = A;
+    for (int i = 1; i < B; i++) result *= A;
+    return result;
+}
+
+// Метод ввода int
+int InputInteger(string messageText)
+{
+    bool isNumber = false; int number = -1;
+    while (!isNumber)
+    {
+        System.Console.WriteLine(messageText);
+        isNumber = int.TryParse(Console.ReadLine(), out number);
+    }
+    return number;
+}
