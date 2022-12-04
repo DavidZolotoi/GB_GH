8321081a705b68634403fd4a725163db73b2fd8f
David <zolotoi77@inbox.ru>
2022-12-04 22:45:56 +0300

    Д/З по знакомству с языками. Урок 4 - Функции. Сделал задачу №2.

--- q01m06_CsharpStart/Homework04_02/Program.cs
+++ q01m06_CsharpStart/Homework04_02/Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Задача 2: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе. Реализовать через функции.");
+
+int number = InputInteger($"Введите число в промежутке от {int.MinValue} по {int.MaxValue}");
+System.Console.WriteLine(SummNumbers(number));
+
+int SummNumbers(int number)
+{
+    if (number < 0) number = -number;
+    int s = 0;
+    while (number > 0)
+    {
+        s += number % 10;
+        number /= 10;
+    }
+    return s;
+}
+
+// Метод ввода int
+int InputInteger(string messageText)
+{
+    bool isNumber = false; int number = -1;
+    while (!isNumber)
+    {
+        System.Console.WriteLine(messageText);
+        isNumber = int.TryParse(Console.ReadLine(), out number);
+    }
+    return number;
+}
