cc0c804246ffb2c6cc8464a66cb9d7f2f7ac9f82
David <zolotoi77@inbox.ru>
2022-12-11 11:58:06 +0300

    К/Р по знакомству с языками. Урок 6-7. Оодномерные и двумерные массивы

---
+++ q01m06_CsharpStart/Classwork07_05/Program.cs
@@ -0,0 +1,51 @@
+﻿// Задайте двумерный массив. Найдите сумму элементов главной диагонали.
+// Например, задан массив:
+// 1   4   7
+// 5   9   2
+// 8   4   2
+// Сумма элементов главной диагонали: 1+9+2 = 12
+
+int n = Prompt("Количество строк и столбцов");
+int[,] array = FillCreateArray(n, n);
+System.Console.WriteLine("Массив:");
+PrintArray(array);
+System.Console.WriteLine($"Сумма элементов по диагонали: {GetSummDiagonal(array)}");
+
+int GetSummDiagonal(int[,] array)
+{
+    int summ = 0;
+    for (int i = 0; i < array.GetLength(1); i++)
+        summ += array[i, i];
+    return summ;
+}
+
+void PrintArray(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            System.Console.Write($"{matr[i, j]}\t");
+        }
+        System.Console.WriteLine();
+    }
+}
+
+int[,] FillCreateArray(int m, int n)
+{
+    int[,] array = new int[m, n];
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(1, 11);
+        }
+    }
+    return array;
+}
+
+int Prompt(string message)
+{
+    Console.Write($"{message} >");              // Вывод приглашения
+    return Convert.ToInt32(Console.ReadLine()); // ввод числа
+}
---
+++ q01m06_CsharpStart/Classwork07_06/Program.cs
@@ -0,0 +1,58 @@
+﻿int Prompt(string message)
+{
+    Console.Write($"{message} >");              // Вывод приглашения
+    return Convert.ToInt32(Console.ReadLine()); // ввод числа
+}
+
+int[,] FillCreateArray(int m, int n)
+{
+    int[,] array = new int[m, n];
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(1, 10);
+        }
+    }
+    return array;
+}
+
+void PrintArray(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            System.Console.Write($"{matr[i, j]}\t");
+        }
+        System.Console.WriteLine();
+    }
+}
+
+(int?, int?) SearchElement(int[,] matr, int SearchEl)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            if (SearchEl == matr[i, j])
+            {
+                return (i, j);
+            }
+        }
+    }
+    return (null, null);
+}
+
+int rows = Prompt("Input number of rows > ");
+int coloums = Prompt("Input number of coloums > ");
+int[,] Array = FillCreateArray(rows, coloums);
+PrintArray(Array);
+int SeachEl = Prompt("Input element > ");
+(int? FoundedRow, int? FoundedColoumn) = SearchElement(Array, SeachEl);
+
+if (FoundedRow == null)
+{
+    Console.WriteLine($"Element is not founded");
+}
+else Console.WriteLine($"Row and Coloumn is {FoundedRow} and {FoundedColoumn}");
