430909d4966f13037f53b48eebcfc37f06cfc443
David <zolotoi77@inbox.ru>
2022-12-11 13:33:56 +0300

    Немного поправил .bat файлы

--- 0.commands.bat
+++ 0.commands.bat
@@ -1,3 +1,6 @@
-echo off
-echo ".bat files:
-1.AddCommitPush.bat ^"...^" -> git add, commit, push"
+rem *.bat files:
+rem 1.AddCommitPush.bat	"..."		-> git add, commit, push"
+rem 2.
+rem 3.
+rem 4.
+rem 5.
