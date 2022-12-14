895a2865547d4f849866384dcfda7c8b5cc5ea93
David <zolotoi77@inbox.ru>
2022-12-15 00:50:04 +0300

    Д/З по знакомству с языками. Урок 7. Двумерные массивы. Почти доделал задачу №3.

--- q01m06_CsharpStart/07/Homework07_03/Program.cs
+++ q01m06_CsharpStart/07/Homework07_03/Program.cs
@@ -1,2 +1,45 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 3. Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+Console.WriteLine("Задача 3. Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.");
+(int m, int n) sizeArray = (new Random().Next(5, 10), new Random().Next(5, 10)); // Размеры массива
+int[,] array = GetRandomIntArray2(sizeArray.m, sizeArray.n);
+PrintArray2(array);
+GetColumnAvg(array);
+
+
+// Метод, возвращающий массив средних арифм. по столбцам от двумерного массива
+double[] GetColumnAvg(int[,] array)
+{
+    double[] avg = new double[array.GetLength(1)];              // кол-во колонок
+    for (int i = 0; i < array.GetLength(1); i++)                 // движение по колонкам (вправо)
+    {
+        avg[i]=0;
+        for (int j = 0; j < array.GetLength(0); j++)            // движение по строке (вниз)
+        {
+                avg[i] += array[j, i];
+        }
+        avg[i] = avg[i] / (double)array.GetLength(0);
+        System.Console.Write($"{avg[i]:f2}\t");
+    }
+    return avg;
+}
+
+// Генерация массива с итами
+int[,] GetRandomIntArray2(int m = 5, int n = 5)   // в теории передавать и возвращать необязательно, но так удобнее
+{
+    int[,] array = new int[m, n];
+    for (int i = 0; i < array.GetLength(0); i++)
+        for (int j = 0; j < array.GetLength(1); j++)
+            array[i, j] = new Random().Next(0, 10);
+    return array;
+}
+
+// Метод вывода 2хмерного массива
+void PrintArray2(int[,] array)
+{
+    System.Console.WriteLine($"Размеры массива: {array.GetLength(0)} х {array.GetLength(1)}.");
+    for (int i = 0; i < array.GetLength(0); i++)                 // движение по строкам (вниз)
+    {
+        for (int j = 0; j < array.GetLength(1); j++) System.Console.Write($"{array[i, j]}\t");  // движение по колонкам (вправо) + вывод
+        System.Console.WriteLine();
+    }
+}
