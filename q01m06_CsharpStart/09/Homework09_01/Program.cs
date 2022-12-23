ce1c6297338884c0a39c04cfebc04e6002100365
David <zolotoi77@inbox.ru>
2022-12-23 16:14:12 +0300

    Д/З по знакомству с языками. Урок 9 - Рекурсия. Сделал задачу №1.

--- q01m06_CsharpStart/09/Homework09_01/Program.cs
+++ q01m06_CsharpStart/09/Homework09_01/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 1: Задайте значения M и N. Напишите программу, которая выведет все чётные натуральные числа в промежутке от M до N с помощью рекурсии.
+Console.WriteLine("Задача 1: Задайте значения M и N. Напишите программу, которая выведет все чётные натуральные числа в промежутке от M до N с помощью рекурсии.");
+(int start, int finish) range = (new Random().Next(1, 11), new Random().Next(10, 21));
+System.Console.WriteLine($"Выбран диапозон с {range.start} по {range.finish}.");
+string txt = recurs(range); // результат работы рекурсивного метода
+System.Console.WriteLine(txt);
+
+// Рекурсивный способ выведения четных чисел в промежутке
+string recurs((int start, int finish) range, string outputText="Четные числа в указанном диапазоне: ")
+{
+    if(range.start % 2 == 0)
+        outputText += range.start.ToString() + "  ";
+    range.start += 1;
+    if (range.start > range.finish) return outputText;
+    return recurs(range, outputText);
+}
