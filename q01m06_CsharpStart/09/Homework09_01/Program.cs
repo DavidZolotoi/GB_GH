21b43babc8eec46b95150f481d609b40d52ce1ce
DavidZolotoi <80065499+DavidZolotoi@users.noreply.github.com>
2022-12-25 00:52:11 +0300

    Работа над ошибками, выявленными преподавателем (№1)
    Урок 9, Задача №1

--- q01m06_CsharpStart/09/Homework09_01/Program.cs
+++ q01m06_CsharpStart/09/Homework09_01/Program.cs
@@ -1,13 +1,13 @@
 ﻿// Задача 1: Задайте значения M и N. Напишите программу, которая выведет все чётные натуральные числа в промежутке от M до N с помощью рекурсии.
 Console.WriteLine("Задача 1: Задайте значения M и N. Напишите программу, которая выведет все чётные натуральные числа в промежутке от M до N с помощью рекурсии.");
 (int start, int finish) range = (new Random().Next(1, 11), new Random().Next(10, 21));
-System.Console.WriteLine($"Четные числа в диапазоне с {range.start} по {range.finish}: {recurs(range)}");
+System.Console.WriteLine($"Четные числа в диапазоне с {range.start} по {range.finish}: {ShowEvenNumbers(range)}");
 
 // Рекурсивный способ выведения четных чисел в промежутке
-string recurs((int start, int finish) range, string outputText="")
+string ShowEvenNumbers((int start, int finish) range, string outputText="")
 {
     if(range.start % 2 == 0) outputText += range.start.ToString() + "  ";
     range.start += 1;
     if (range.start > range.finish) return outputText;
-    return recurs(range, outputText);
+    return ShowEvenNumbers(range, outputText);
 }
