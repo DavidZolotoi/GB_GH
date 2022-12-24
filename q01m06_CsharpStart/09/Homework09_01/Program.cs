e062320b19c6784c301ae860305d0b7ab903bf78
David <zolotoi77@inbox.ru>
2022-12-24 18:28:03 +0300

    Д/З по знакомству с языками. Урок 9 - Рекурсия. Сделал задачу №2. И немного скорректировал №1

--- q01m06_CsharpStart/09/Homework09_01/Program.cs
+++ q01m06_CsharpStart/09/Homework09_01/Program.cs
@@ -1,15 +1,12 @@
 ﻿// Задача 1: Задайте значения M и N. Напишите программу, которая выведет все чётные натуральные числа в промежутке от M до N с помощью рекурсии.
 Console.WriteLine("Задача 1: Задайте значения M и N. Напишите программу, которая выведет все чётные натуральные числа в промежутке от M до N с помощью рекурсии.");
 (int start, int finish) range = (new Random().Next(1, 11), new Random().Next(10, 21));
-System.Console.WriteLine($"Выбран диапозон с {range.start} по {range.finish}.");
-string txt = recurs(range); // результат работы рекурсивного метода
-System.Console.WriteLine(txt);
+System.Console.WriteLine($"Четные числа в диапазоне с {range.start} по {range.finish}: {recurs(range)}");
 
 // Рекурсивный способ выведения четных чисел в промежутке
-string recurs((int start, int finish) range, string outputText="Четные числа в указанном диапазоне: ")
+string recurs((int start, int finish) range, string outputText="")
 {
-    if(range.start % 2 == 0)
-        outputText += range.start.ToString() + "  ";
+    if(range.start % 2 == 0) outputText += range.start.ToString() + "  ";
     range.start += 1;
     if (range.start > range.finish) return outputText;
     return recurs(range, outputText);
--- q01m06_CsharpStart/09/Homework09_02/Program.cs
+++ q01m06_CsharpStart/09/Homework09_02/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 2: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N с помощью рекурсии.
+Console.WriteLine("Задача 2: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N с помощью рекурсии.");
+(int start, int finish) range = (new Random().Next(-20, 0), new Random().Next(0, 21));
+System.Console.WriteLine($"Сумма натуральных в диапазоне с {range.start} по {range.finish} = {recurs(range)}");
+
+// Рекурсивный способ выведения четных чисел в промежутке
+// Судя по википедии в мире между математиками нет единой договоренности о том, включать 0 в натуральные или нет
+// Но написано, что в РФ в основном НЕ включают.
+long recurs((int start, int finish) range, long Snatural = 0)
+{
+    if(range.start > 0) Snatural += range.start;
+    range.start += 1;
+    if (range.start > range.finish) return Snatural;
+    return recurs(range, Snatural);
+}
