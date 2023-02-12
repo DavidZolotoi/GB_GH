7b6dc4c9634d38c0024aef813511637ac1a1b780
David <zolotoi77@inbox.ru>
2023-02-12 15:59:23 +0300

    Д/З по знакомству с Python. рок 3. Списки и словари. Задача 4

---
+++ q01m08_Python/03/hw03_04/hw03_04.py
@@ -0,0 +1,26 @@
+#     Напишите программу, которая будет преобразовывать десятичное число в двоичное.
+#     Пример:
+# - 45 -> 101101
+# - 3 -> 11
+# - 2 -> 10
+
+import random
+
+# Генерация числа в пределах от A до B
+A = 1
+B = 50
+randomInt10 = random.randint(A, B + 1)
+print(f"В десятичной с.ч.: {randomInt10}")
+
+# Перевод числа в двоичную систему счисления
+randomStrForReverse2 = ''
+while randomInt10 > 0:
+        randomStrForReverse2 += str(randomInt10 % 2)
+        randomInt10 = randomInt10 // 2
+
+# Реверс текста
+randomStr2 = ''
+for i in range(0, len(randomStrForReverse2)):
+        randomStr2 += randomStrForReverse2[len(randomStrForReverse2) - i - 1]
+
+print(f"В двоичной с.ч.: {randomStr2} ")
