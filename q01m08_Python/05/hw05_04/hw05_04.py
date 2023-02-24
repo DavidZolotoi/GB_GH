1a31eb6be5044bdb0ecb5ff11e9e67d4451e4fd3
David <zolotoi77@inbox.ru>
2023-02-24 03:40:22 +0300

    Д/З по знакомству с Python. Урок 5. Рекурсия и алгоритмы. Задача №4 (начало)

--- q01m08_Python/05/hw05_04/hw05_04.py
+++ q01m08_Python/05/hw05_04/hw05_04.py
@@ -1,6 +1,11 @@
-def ddd(txt):
-    txt = "ddd"
-
-txt = "hhh"
-ddd(txt)
-print(txt)
+txt = "qwweeerrrrttttt"
+simbDict = {}
+simb = txt[0]
+simbDict[simb] = 1
+for i in range(1, len(txt)):
+	if txt[i] == simb:
+		simbDict[simb] += 1
+	else:
+		simb = txt[i]
+		simbDict[simb] = 1
+print(simbDict)
