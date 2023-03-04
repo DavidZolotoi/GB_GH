9aee4cd041b3e8a6f3c13d3c19114c12fc4086fc
David <zolotoi77@inbox.ru>
2023-03-04 21:37:52 +0300

    Д/З по Java: знакомство и как пользоваться базовым API. Урок 1. накомство с языком программирования Java Задача №1

--- q01m09_Java/01/hw01_01/hw01_01.java
+++ q01m09_Java/01/hw01_01/hw01_01.java
@@ -9,12 +9,16 @@ public class hw01_01 {
         Scanner scanner = new Scanner(System.in);
         Integer inputInt = GetInputInt(scanner, "Введите число: ");
         Integer Tn = 0;
+        Long F = (long) 1;
         for (int i = 1; i <= inputInt; i++) {
             Tn += i;
+            F *= i;
         }
         System.out.printf("Треугольное число, вычисленное через цикл: %d", Tn);
         System.out.println();
         System.out.printf("Треугольное число, вычисленное по формуле: %d", inputInt*(inputInt+1)/2);
+        System.out.println();
+        System.out.printf("Факториял, вычисленный через цикл: %d", F);
         // Всё то, что необходимо закрыть в конце программы
         scanner.close();
     }
