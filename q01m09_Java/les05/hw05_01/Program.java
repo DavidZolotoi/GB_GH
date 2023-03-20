57c77cd59b696cc63a7276479462f610b6e4aa49
David <zolotoi77@inbox.ru>
2023-03-20 19:01:21 +0300

    Д/З по Java: знакомство и как пользоваться базовым. Урок 5. Хранение и обработка данных ч2: множество коллекций Map. Задача №1 (продолжение)

--- q01m09_Java/les05/hw05_01/Program.java
+++ q01m09_Java/les05/hw05_01/Program.java
@@ -15,30 +15,87 @@ public class Program
     public static void main(String[] args)
     {
         // Создать файл с указанным именем рядом с исполняемым файлом
-        String phonebookPath = GetPathForNewFile("Phonebook.csv");
+        String phoneBookPath = GetPathForNewFile("PhoneBook.csv");
         // Сканер
         Scanner scanner = new Scanner(System.in);
-        // Загрузить справочник из файла в строку
-        StringBuilder phoneMapTxt = new StringBuilder();
-        GetPhoneMapTxt(phonebookPath, phoneMapTxt);     // загрузка в phoneMapTxt
-        // Создать словарь и заполнить его из строки
-        HashMap<String, LinkedList<String>> phoneMap = new HashMap<>();
-        GetPhoneMap(phoneMap, phoneMapTxt.toString());  // расчленить и добавить в словарь
+        // Прочитать файл в строку и расчленить ее на словарь
+        HashMap<String, LinkedList<String>> phoneMap = GetPhoneMap(GetPhoneMapTxt(phoneBookPath));
         //System.out.println(phoneMap.get("Lastname1 F.").get(0));    // проверка
-
-        while (true)
+        
+        // Работа в меню
+        Boolean isWork = true;
+        while (isWork)
         {
             System.out.println(GetMenu(scanner));
-            if (true) break;
+            System.out.println("Выберите пункт меню: ");
+            Integer menuItem = Integer.parseInt(scanner.nextLine());
+            switch (menuItem)
+            {
+                case 5: System.out.println("Всего доброго!\n"); isWork = false; break;
+                case 1: System.out.println(ShowPhoneBook(phoneBookPath)); break;
+                case 2: System.out.println(SearchContact(scanner, phoneBookPath)); break;
+                //case 3: AddContact(); break;
+                //case 4: DelContact(); break;
+                default: break;
+            }
+            
         }
 
         // Уничтожить сканер
         scanner.close();
     }
 
-    // Преобразовать строку (полученную из файла) в словарь
-    private static void GetPhoneMap(HashMap<String, LinkedList<String>> phoneMap, String phoneMapTxt)
+    // Поиск контакта -> отчет с данными
+    private static String SearchContact(Scanner scanner, String phoneBookPath)
     {
+        HashMap<String, LinkedList<String>> phoneMap = GetPhoneMap(GetPhoneMapTxt(phoneBookPath));
+        System.out.println("Введите строку, по которой необходимо произвести посик: ");
+        String txtForSearch = scanner.nextLine();
+        String contactInfo = "Ничего не найдено.";
+        StringBuilder findText = new StringBuilder();
+        for (var contact : phoneMap.entrySet())
+        {
+            StringBuilder contactToString = new StringBuilder();
+            contactToString.append(String.format("Имя: %s:\n", contact.getKey()));
+            Integer countNumbers = 1;
+            for (var numbers : phoneMap.get(contact.getKey()))
+            {
+                contactToString.append(String.format("Телефон №%d: %s;\n", countNumbers, numbers));
+                countNumbers += 1;
+            }
+            
+            if (contactToString.indexOf(txtForSearch) == -1)
+                continue;
+            else
+                findText.append(contactToString);
+        }
+        if (findText.length()>0) return findText.toString();
+        else return contactInfo;
+    }
+
+    // Показ справочника = возврат в виде строки
+    private static String ShowPhoneBook(String phoneBookPath)
+    {
+        StringBuilder phoneMapToString = new StringBuilder();
+        phoneMapToString.append("Содержимое справочника: \n");
+        HashMap<String, LinkedList<String>> phoneMap = GetPhoneMap(GetPhoneMapTxt(phoneBookPath));
+        for (var contact : phoneMap.entrySet())
+        {
+            phoneMapToString.append(String.format("Имя: %s:\n", contact.getKey()));
+            Integer countNumbers = 1;
+            for (var numbers : phoneMap.get(contact.getKey()))
+            {
+                phoneMapToString.append(String.format("Телефон №%d: %s;\n", countNumbers, numbers));
+                countNumbers += 1;
+            }
+        }
+        return phoneMapToString.toString();
+    }
+
+    // Преобразовать строку (полученную из файла) в словарь
+    private static HashMap<String, LinkedList<String>> GetPhoneMap(String phoneMapTxt)
+    {
+        HashMap<String, LinkedList<String>> phoneMap = new HashMap<>();
         String[] rows = phoneMapTxt.split("\n");
         for (int i = 0; i < rows.length; i++)               // прогон по строкам
         {
@@ -58,12 +115,14 @@ public class Program
             }
             phoneMap.put(contactName, contactNumbers);
         }
+        return phoneMap;
     }
 
     // Метод, считывающий данные из файла в StringBuilder
-    private static void GetPhoneMapTxt(String phonebookPath, StringBuilder phoneMapTxt)
+    private static String GetPhoneMapTxt(String phoneBookPath)
     {
-        try(FileReader reader = new FileReader(phonebookPath))
+        StringBuilder phoneMapTxt = new StringBuilder();
+        try(FileReader reader = new FileReader(phoneBookPath))
         {
            // читаем посимвольно
             int c;
@@ -76,13 +135,20 @@ public class Program
         {
             System.out.println(ex.getMessage());
         }
+        return phoneMapTxt.toString();
     }
 
     // вернуть меню
     private static String GetMenu(Scanner scanner)
     {
         StringBuilder menuTxt = new StringBuilder();
-        menuTxt.append("This is menu.");
+        menuTxt.append("******************************\n");
+        menuTxt.append("Меню справочника:\n");
+        menuTxt.append("1. Показать всё.\n");
+        menuTxt.append("2. Найти контакт.\n");
+        menuTxt.append("3. Добавить контакт.\n");
+        menuTxt.append("4. Удалить контакт.\n");
+        menuTxt.append("5. Завершить работу программы.\n");
         return menuTxt.toString();
     }
 
