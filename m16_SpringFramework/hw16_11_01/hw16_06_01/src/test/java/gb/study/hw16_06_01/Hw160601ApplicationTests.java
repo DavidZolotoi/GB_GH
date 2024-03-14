6808fcdc2f286bbd535498d98dad11aed5445b26
David <zolotoi77@inbox.ru>
2024-03-14 03:21:23 +0300

    Д/З. Тема: Фреймворк Spring. Урок 11. Spring Actuator. Настройка мониторинга с Prometheus и Grafana.

---
+++ m16_SpringFramework/hw16_11_01/hw16_06_01/src/test/java/gb/study/hw16_06_01/Hw160601ApplicationTests.java
@@ -0,0 +1,13 @@
+package gb.study.hw16_06_01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Hw160601ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
